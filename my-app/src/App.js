8844322002ce406c0bd41ab79ed07da0b803396f
Oleh Chychkevych <>
2020-03-19 22:59:07 +0200

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import theme from './app.module.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={theme.appContainer}>
+        <header>
+          Simple Ag-grid table
+        </header>
+        {/* Write your code bellow, you can split your code as you wish*/}
+      </div>
+    );
+  }
+}
+
+export default App;
